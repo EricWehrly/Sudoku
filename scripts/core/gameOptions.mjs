135ea8a263df8561464f9abe1e8e4577004c0d49
EricWehrly <eric@disruptiontheory.com>
2024-03-03 13:41:43 -0500

    use class-level setter

--- scripts/core/gameOptions.mjs
+++ scripts/core/gameOptions.mjs
@@ -2,21 +2,33 @@
 
 export default class GameOption {
 
+    #name;
+    #value;
+
+    get name() { return this.#name; }
+
     constructor(options) {
 
-        let bValue = options.value;
+        this.#name = options.name;
+        this.#value = options.value;
+
         // maybe we can move a bunch of this logic to a 'Listed' class ...
+        const that = this;
         Object.defineProperty(GameOption, options.name, {
             // value: options.value,
             // writable: true,
             // enumerable: true,
             // configurable: true,
             get() {
-                return bValue;
+                return that.#value;
             },
             set(newValue) {
-                bValue = newValue;
+                that.setter(newValue);
             },
         });
+    }
+
+    setter(newValue) {
+        this.#value = newValue;
     }
 }
