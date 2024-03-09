657f19d048ec7d6fb1226684526f070847a97c7b
Eric Wehrly <eric.wehrly@gmail.com>
2024-03-09 12:36:46 -0500

    activate a random malefactor when a square is correct

--- scripts/gameObjects/malefactors/malefactors.mjs
+++ scripts/gameObjects/malefactors/malefactors.mjs
@@ -4,9 +4,14 @@ import SquareFreeze from "./squareFreeze.mjs";
 
 function squareCorrect(square) {
 
-    // do random malefactor
-    // Malefactors.CellFlame.enable();
-    Malefactors.SquareFreeze.enable();
+    let malefactors = Object.values(Malefactors);
+    malefactors = malefactors.filter(malefactor => malefactor.enabled == false);
+    const index = Math.randomBetween(0, malefactors.length);
+    const randomMalefactor = malefactors[index];
+    
+    console.log(randomMalefactor);
+
+    if(randomMalefactor) randomMalefactor.enable();
 }
 
 Events.Subscribe(Events.List.SudokuSquareCorrect, squareCorrect);
