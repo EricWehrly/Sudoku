a28f4479b9b8125f40b79523b9e01936a66775b2
Eric Wehrly <eric.wehrly@gmail.com>
2024-03-19 01:01:22 -0400

    push element creation down to a utility class

--- scripts/rendering/dom/ui/uiElement.mjs
+++ scripts/rendering/dom/ui/uiElement.mjs
@@ -1,33 +1,21 @@
+import { createElement } from "../../../util/javascript-extensions.mjs";
 import Renderer from "../renderer.mjs";
 
 export default class UIElement extends Renderer {
 
-    #parent;
     #name;
     get name() { return this.#name; }
 
     constructor(options = {}) {
+
+        if(!options.parent) options.parent = "ui-container-parent";
+        options.element = createElement({
+            classes: `ui ${options.name || ''} ${options.classes || ''}`,
+            parent: options.parent
+        });
         
-        options.element = document.createElement('div');
-        options.element.className = `ui ${options.name || ''} ${options.classes || ''}`;
         super(options);
 
         this.#name = options.name;
-        
-        if(options.parent) options.parent.appendChild(options.element);
-        else {
-            this.#parent = "ui-container-parent";
-            this.#attachRoot();
-        }
-    }
-
-    #attachRoot() {
-        const renderRoot = document.getElementById(this.#parent);
-        if(renderRoot) {
-            renderRoot.appendChild(this.element);
-        } else {
-            // bleh, hack
-            setTimeout(this.#attachRoot.bind(this), 10);
-        }
     }
 }
